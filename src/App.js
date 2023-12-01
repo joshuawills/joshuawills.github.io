9f8f7598bb5ac61b81777850c3e6a20a52839caf
joshuawills <joshuamatthewwills@gmail.com>
2023-12-01 17:08:58 +1100

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import '../App.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <h1 style={{ margin: 0 }}>@joshuawills</h1>
+        </div>
+    );
+}
+
+export default Header;
